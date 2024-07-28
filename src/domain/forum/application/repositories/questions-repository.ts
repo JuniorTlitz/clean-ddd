93cea1343e20e8d44f90bdb8e586a782df42d0a3
Tarcizio Junior <tarcizio.junior2318@outlook.com>
2024-07-28 11:11:01 -0300

    feat: create delete and findById methods

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -3,4 +3,6 @@ import { Question } from '../../enterprise/entities/questions'
 export interface QuestionRepository {
   findBySlug(slug: string): Promise<Question | null>
   create(questions: Question): Promise<void>
+  delete(question: Question): Promise<void>
+  findById(id: string): Promise<Question | null>
 }
