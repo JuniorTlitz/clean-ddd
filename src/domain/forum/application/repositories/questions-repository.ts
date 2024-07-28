79520b851a1fb16e2f0bd62a04934a184c1c31c7
Tarcizio Junior <tarcizio.junior2318@outlook.com>
2024-07-28 10:07:13 -0300

    feat: create new method to question repository

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,5 +1,6 @@
 import { Question } from '../../enterprise/entities/questions'
 
 export interface QuestionRepository {
+  findBySlug(slug: string): Promise<Question | null>
   create(questions: Question): Promise<void>
 }
