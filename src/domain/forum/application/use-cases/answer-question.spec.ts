02dc1a874f190985cd96974f171e0c30c19c2bd9
Tarcizio Santos <62338122+JuniorTlitz@users.noreply.github.com>
2024-07-27 21:28:11 -0300

    refactor: change folders

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,6 +1,6 @@
 import { AnswerQuestionUseCase } from './answer-question'
-import { AnswerRepository } from '../repositories/answer-repository'
-import { Answer } from '../entities/answer'
+import { AnswerRepository } from '@/domain/forum/application/repositories/answer-repository'
+import { Answer } from '../../enterprise/entities/answer'
 
 const fakeAnswersRepository: AnswerRepository = {
   create: async (answer: Answer) => {
